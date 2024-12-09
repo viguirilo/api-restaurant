990e39fd6249449b97c3704ae49d9f0039c5c103
Vitor Lopes <viguirilo@gmail.com>
2024-12-09 12:45:54 -0300

    Adding entities

---
+++ src/main/java/com/restaurant/api/rest/v1/entity/Permission.java
@@ -0,0 +1,39 @@
+package com.restaurant.api.rest.v1.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import com.restaurant.api.rest.v1.vo.PermissionRequestVO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "permission")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@JsonRootName(value = "permission")
+public class Permission implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty(value = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    @JsonProperty(value = "name")
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    @JsonProperty(value = "description")
+    private String description;
+
+    public Permission(PermissionRequestVO permissionRequestVO) {
+        this.name = permissionRequestVO.getName();
+        this.description = permissionRequestVO.getDescription();
+    }
+
+}
---
+++ src/main/java/com/restaurant/api/rest/v1/entity/User.java
@@ -0,0 +1,63 @@
+package com.restaurant.api.rest.v1.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import com.restaurant.api.rest.v1.vo.UserRequestVO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@JsonRootName(value = "user")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty(value = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    @JsonProperty(value = "name")
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    @JsonProperty(value = "email")
+    private String email;
+
+    @JsonIgnore
+    @Column(name = "password", nullable = false)
+    @JsonProperty(value = "password")
+    private String password;
+
+    @CreationTimestamp
+    @Column(name = "creation_date", nullable = false, columnDefinition = "datetime")
+    @JsonProperty(value = "creationDate")
+    private LocalDateTime creationDate;
+
+    @ManyToMany
+    @JoinTable(name = "user_group",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "group_id")
+    )
+    private List<Group> groups = new ArrayList<>();
+
+    public User(UserRequestVO userRequestVO) {
+        this.name = userRequestVO.getName();
+        this.email = userRequestVO.getEmail();
+        this.password = userRequestVO.getPassword();
+        this.creationDate = LocalDateTime.now();
+    }
+
+}
