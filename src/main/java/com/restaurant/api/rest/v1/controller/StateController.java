37f459dc89d1ac9b524525992921a6ec607214e6
Vitor Lopes <viguirilo@gmail.com>
2024-12-09 11:28:00 -0300

    Adding State related files and endpoints

---
+++ src/main/java/com/restaurant/api/rest/v1/controller/StateController.java
@@ -0,0 +1,76 @@
+package com.restaurant.api.rest.v1.controller;
+
+import com.restaurant.api.rest.v1.entity.State;
+import com.restaurant.api.rest.v1.service.StateService;
+import com.restaurant.api.rest.v1.vo.StateRequestVO;
+import com.restaurant.api.rest.v1.vo.StateResponseVO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rest/v1/states")
+@RequiredArgsConstructor
+public class StateController {
+
+    private final StateService stateService;
+
+    // TODO(colocar este método para retornar um VO)
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseStatus(HttpStatus.CREATED)
+    public State save(@RequestBody StateRequestVO stepRequestVO) {
+        return stateService.save(stepRequestVO);
+    }
+
+    // TODO(colocar paginação neste endpoint)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<State>> findAll() {
+        return ResponseEntity.ok().body(stateService.findAll());
+    }
+
+    @GetMapping(
+            value = "/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<StateResponseVO> findById(@PathVariable Long id) {
+        StateResponseVO stateResponseVO = stateService.findById(id);
+        if (stateResponseVO == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok().body(stateResponseVO);
+    }
+
+    // TODO(colocar este método para retornar um VO)
+    @PutMapping(
+            value = "/{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<State> update(@PathVariable Long id, @RequestBody StateRequestVO stateRequestVO) {
+        State state = stateService.update(id, stateRequestVO);
+        if (state == null) return ResponseEntity.notFound().build();
+        else return ResponseEntity.ok().body(state);
+    }
+
+    // TODO(colocar este método para retornar um VO)
+    @DeleteMapping(
+            value = "/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        try {
+            State state = stateService.delete(id);
+            if (state == null) return ResponseEntity.notFound().build();
+            else return ResponseEntity.noContent().build();
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+    }
+
+}
---
+++ src/main/java/com/restaurant/api/rest/v1/service/StateService.java
@@ -0,0 +1,51 @@
+package com.restaurant.api.rest.v1.service;
+
+import com.restaurant.api.rest.v1.entity.State;
+import com.restaurant.api.rest.v1.repository.StateRepository;
+import com.restaurant.api.rest.v1.vo.StateRequestVO;
+import com.restaurant.api.rest.v1.vo.StateResponseVO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class StateService {
+
+    private final StateRepository stateRepository;
+
+    public State save(StateRequestVO stepRequestVO) {
+        return stateRepository.save(new State(stepRequestVO));
+    }
+
+    public List<State> findAll() {
+        return stateRepository.findAll();
+    }
+
+    public StateResponseVO findById(Long id) {
+        Optional<State> stateOptional = stateRepository.findById(id);
+        return stateOptional.map(StateResponseVO::new).orElse(null);
+    }
+
+    public State update(Long id, StateRequestVO stateRequestVO) {
+        Optional<State> stateOptional = stateRepository.findById(id);
+        if (stateOptional.isPresent()) {
+            State state = stateOptional.get();
+            BeanUtils.copyProperties(stateRequestVO, state, "id");
+            return stateRepository.save(state);
+        } else return null;
+    }
+
+    public State delete(Long id) {
+        Optional<State> stateOptional = stateRepository.findById(id);
+        if (stateOptional.isPresent()) {
+            State state = stateOptional.get();
+            stateRepository.delete(state);
+            return state;
+        } else return null;
+    }
+
+}
