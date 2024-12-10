628d2576cc70dc7813eedee370326f1fe707bda7
Vitor Lopes <viguirilo@gmail.com>
2024-12-10 18:16:14 -0300

    Updating table payment_method, adding PIX as a new entry and reordering

--- src/main/resources/db/migration/V0013__inserting-payment-methods.sql
+++ src/main/resources/db/migration/V0013__inserting-payment-methods.sql
@@ -1,5 +1,6 @@
-INSERT INTO payment_method (description) VALUES ('CREDIT_CARD');
-INSERT INTO payment_method (description) VALUES ('DEBIT_CARD');
 INSERT INTO payment_method (description) VALUES ('CASH');
+INSERT INTO payment_method (description) VALUES ('DEBIT_CARD');
+INSERT INTO payment_method (description) VALUES ('CREDIT_CARD');
+INSERT INTO payment_method (description) VALUES ('PIX');
 INSERT INTO payment_method (description) VALUES ('CHECK');
 INSERT INTO payment_method (description) VALUES ('CRYPTO_COINS');
