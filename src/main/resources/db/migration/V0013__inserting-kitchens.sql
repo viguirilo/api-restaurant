dc7cee0dead848c5b5208543acfde522dd521267
Vitor Lopes <viguirilo@gmail.com>
2024-12-14 09:38:37 -0300

    Implementing Flyway afterMigrate methodology

--- src/main/resources/db/migration/V0013__inserting-kitchens.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO kitchen (name) VALUES ('Thai');
-INSERT INTO kitchen (name) VALUES ('Indian');
-INSERT INTO kitchen (name) VALUES ('Brazilian');
-INSERT INTO kitchen (name) VALUES ('Japanese');
-INSERT INTO kitchen (name) VALUES ('Italian');
-INSERT INTO kitchen (name) VALUES ('Australian');
-INSERT INTO kitchen (name) VALUES ('Mexican');
-INSERT INTO kitchen (name) VALUES ('Chilean');
