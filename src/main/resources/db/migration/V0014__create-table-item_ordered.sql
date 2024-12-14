5bdaf7cee5fbe02b68fe6b5b781f09c70b14b5e8
Vitor Lopes <viguirilo@gmail.com>
2024-12-14 10:03:13 -0300

    Refactoring columns into database

--- src/main/resources/db/migration/V0014__create-table-item_ordered.sql
+++ src/main/resources/db/migration/V0014__create-table-item_ordered.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `item_ordered` (
   `id` bigint NOT NULL AUTO_INCREMENT,
+  `order_id` bigint NOT NULL,
+  `product_id` bigint NOT NULL,
   `quantity` int NOT NULL,
   `unit_price` decimal(38,2) NOT NULL,
   `total_price` decimal(38,2) NOT NULL,
   `observation` varchar(255) DEFAULT NULL,
-  `order_id` bigint NOT NULL,
-  `product_id` bigint NOT NULL,
   PRIMARY KEY (`id`),
   KEY `order_fk_1` (`order_id`),
   KEY `product_fk_1` (`product_id`),
