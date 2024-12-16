63f507cbebd8da060d5d4509543accae5e5dc4c9
Vitor Lopes <viguirilo@gmail.com>
2024-12-16 15:16:47 -0300

    Adding support to handleHttpMessageNotReadable

--- src/main/java/com/restaurant/api/rest/v1/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/restaurant/api/rest/v1/exceptionHandler/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import com.restaurant.api.rest.v1.exception.EntityNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -25,7 +26,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 BAD_REQUEST.getStatus().value(),
                 BAD_REQUEST.getType(),
                 BAD_REQUEST.getTitle(),
-                ex.getMessage(),
+                BAD_REQUEST.getDetail(),
                 ex.getMessage(),
                 LocalDateTime.now()
         );
@@ -38,7 +39,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 ENTITY_ALREADY_EXISTS.getStatus().value(),
                 ENTITY_ALREADY_EXISTS.getType(),
                 ENTITY_ALREADY_EXISTS.getTitle(),
-                ex.getMessage(),
+                ENTITY_ALREADY_EXISTS.getDetail(),
                 ex.getMessage(),
                 LocalDateTime.now()
         );
@@ -51,7 +52,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 ENTITY_IN_USE.getStatus().value(),
                 ENTITY_IN_USE.getType(),
                 ENTITY_IN_USE.getTitle(),
-                ex.getMessage(),
+                ENTITY_IN_USE.getDetail(),
                 ex.getMessage(),
                 LocalDateTime.now()
         );
@@ -64,15 +65,27 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 ENTITY_NOT_FOUND.getStatus().value(),
                 ENTITY_NOT_FOUND.getType(),
                 ENTITY_NOT_FOUND.getTitle(),
-                ex.getMessage(),
+                ENTITY_NOT_FOUND.getDetail(),
                 ex.getMessage(),
                 LocalDateTime.now()
         );
         return handleExceptionInternal(ex, problemDetail, new HttpHeaders(), ENTITY_NOT_FOUND.getStatus(), webRequest);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest webRequest) {
+        ProblemDetail problemDetail = new ProblemDetail(
+                status.value(),
+                HTTP_MESSAGE_NOT_READABLE.getType(),
+                HTTP_MESSAGE_NOT_READABLE.getTitle(),
+                HTTP_MESSAGE_NOT_READABLE.getDetail(),
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return handleExceptionInternal(ex, problemDetail, new HttpHeaders(), status, webRequest);
+    }
 
-//    TODO(ver aulas 14, 15, 18)
+    //    TODO(ver aulas )
 
     @Override
     protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
