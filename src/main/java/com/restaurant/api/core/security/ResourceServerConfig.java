f6969a094047021faae4fb7b57cf46196c05856c
Vitor Lopes <viguirilo@gmail.com>
2025-02-17 13:39:29 -0300

    Adding resource server with client credentials and opaque token

---
+++ src/main/java/com/restaurant/api/core/security/ResourceServerConfig.java
@@ -0,0 +1,46 @@
+package com.restaurant.api.core.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
+import org.springframework.security.oauth2.server.resource.introspection.SpringOpaqueTokenIntrospector;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
+public class ResourceServerConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.opaque-token.introspection-uri}")
+    String introspectionUri;
+
+    @Value("${spring.security.oauth2.resourceserver.opaque-token.introspection-client-id}")
+    String clientId;
+
+    @Value("${spring.security.oauth2.resourceserver.opaque-token.introspection-client-secret}")
+    String clientSecret;
+
+    @Bean
+    public OpaqueTokenIntrospector opaqueTokenIntrospector() {
+        return new SpringOpaqueTokenIntrospector(introspectionUri, clientId, clientSecret);
+    }
+
+    @Bean
+    public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/oauth2/**").authenticated()
+                                .anyRequest().authenticated()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.opaqueToken(Customizer.withDefaults())
+                ).build();
+    }
+
+}
